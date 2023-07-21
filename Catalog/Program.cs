db66e1f3e12e8765e6fc023c7d393093222303b0
Zhiyuan Liu <thenukezealot@gmail.com>
2023-07-21 22:59:42 +0300

    MongoDB setup

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -1,4 +1,9 @@
 using Catalog.Repositories;
+using Catalog.Settings;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +13,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IItemsRepository, InMemItemsRepository>();
+
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+builder.Services.AddSingleton<IMongoClient>(ServiceProvider =>
+{
+    var settings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+    return new MongoClient(settings.ConnectionString);
+});
+builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
+
 
 var app = builder.Build();
 
--- Catalog/Repositories/IItemsRepository.cs
+++ Catalog/Repositories/IItemsRepository.cs
@@ -4,8 +4,8 @@ namespace Catalog.Repositories
 {
     public interface IItemsRepository
     {
-        Item GetItem(Guid id);
         IEnumerable<Item> GetItems();
+        Item GetItem(Guid id);
         void CreateItem(Item item);
         void UpdateItem(Item item);
         void DeleteItem(Guid id);
